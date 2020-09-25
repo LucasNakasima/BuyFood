0cda8bfabd21759f6f5349a6b5b018220eb30c4e
Lucas Nakasima <lucassoares1847@hotmail.com>
2020-09-25 00:29:34 -0300

    commit buyfood

---
+++ BuyFood/app/src/main/java/com/example/ifarmsplashs/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.ifarmsplashs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        mudaParaActivityMain()
+    }
+
+    private fun mudaParaActivityMain() {
+        val intent = Intent(this, MainActivity::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        }, 4000)
+    }
+
+    fun Intent.change() {
+        startActivity(this)
+        finish()
+    }
+}
